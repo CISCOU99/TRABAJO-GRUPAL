b6ee80bf08824a835727ee81b9893de73f3782ef
DAVID TOALA <davifrank1999@gmail.com>
2025-10-14 18:13:34 -0500

    Add files via upload

---
+++ M_Dinámica_TRABAJO_CLASE/script.js
@@ -0,0 +1,36 @@
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClass();
+        panel.classList.add('active');
+        
+    })
+});
+
+function removeActiveClass(){
+    panels.forEach((panel) => {
+        panel.classList.remove('active');
+    })
+}
+
+//This is the result of how the label looks
+{/* <label>
+  <span style="transition-delay:0ms">E</span>
+  <span style="transition-delay:50ms">m</span>
+  <span style="transition-delay:100ms">a</span>
+  <span style="transition-delay:150ms">i</span>
+  <span style="transition-delay:200ms">l</span>
+</label>; */}
+
+const labels = document.querySelectorAll(".form-control label");
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+    )
+    .join("");
+});
